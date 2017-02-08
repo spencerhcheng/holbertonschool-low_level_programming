abf07d8b4d31e34bfe7ae0cf0f1c3fce8fe2880a
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-02-08 21:43:51 +0000

    CISFUN

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - begins
+ * Return: ends
+*/
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
-}	
+	return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-
-int main()
+/**
+ * main - begin
+ * Return: end
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art\n");
-	return 0;
+	return (0);
 }

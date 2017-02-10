091d730eedfc5bcbc57bb81146da778b33ade606
Spencer <spencerhcheng@gmail.com>
2017-02-10 04:51:51 +0000

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,24 +4,23 @@
  * main - function that prints out
  * a to z in lowercase that omits
  * the characters q and e.
- * return: 0
-*/
+ * Return: 0
+ */
 
 int main(void)
 {
 	char letter = 'a';
 
 	do {
-
 	if (letter == 'e' || letter == 'q')
 	{
 		letter++;
 		continue;
 	}
-
 		putchar(letter);
 		letter++;
 	} while (letter <= 'z');
 	putchar('\n');
+
 	return (0);
 }

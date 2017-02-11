6887d52b853a5ad11ee4813e8e07fe7192fe5a7f
Spencer Cheng <SUPERDRIVE@Spencers-MacBook-Air.local>
2017-02-10 23:25:14 -0800

    add new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all double digit
+ * numbers fro 0 to 99 using 5 putchars
+ * Return:0
+ */
+
+int main(void) 
+{
+	int i = '0';
+	int j = '1';
+
+	while (i <= '8')
+	{
+		while (j <= '9')
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '8' || j!= '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}

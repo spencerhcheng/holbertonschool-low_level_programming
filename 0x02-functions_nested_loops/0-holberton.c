f4009b314e68049ba55aef48316f52369128a1e4
Spencer <spencerhcheng@gmail.com>
2017-02-15 03:23:05 +0000

    update

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * main - Program that prints out
@@ -9,15 +8,16 @@
  */
 
 int main(void)
-
 {
-	char name[] = {"Holberton"};
-	int s;
-
-	for (s = 0; s <= 9; s++)
-	{
-	_putchar(name[s]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }

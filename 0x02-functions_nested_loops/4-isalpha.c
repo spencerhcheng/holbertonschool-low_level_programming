5e1aace424f466aa387f6aaf27cacf57593bd4ab
Spencer <spencerhcheng@gmail.com>
2017-02-15 03:56:02 +0000

    fomatting

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,19 @@
 #include "holberton.h"
 
+/**
+ * _isalpha - function that checks for
+ * alphabetic character.
+ * @c: arguement
+ * lowercase character.
+ * Return: 0
+ */
+
 int _isalpha(int c)
 {
-	if ((c >= 44 && c <= 69) || (c >= 97 && c <= 122))
+	if ((c >= 44 && c <= 69) || (c >= 97 && c <= 122)
 	{
 		return (1);
-
-	} 
+	}
 	else
 	{
 		return (0);

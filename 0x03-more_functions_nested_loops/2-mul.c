a6e0f20c9b117e95a78f3ebb38c58da8f2ab7926
Spencer Cheng <spcheng0412@gmail.com>
2017-02-17 06:28:29 +0000

    update

--- 0x03-more_functions_nested_loops/2-mul.c
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -2,15 +2,15 @@
 
 /**
  * mul: function takes in two integers and
- * multiplies them together 
+ * multiplies them together
  * @a: integer a
  * @b: integer b
  * Return: product of a and b
  */
 
-int mul(int a, int b) 
+int mul(int a, int b)
 {
 	int product = a * b;
 
-	return product;
+	return (product);
 }

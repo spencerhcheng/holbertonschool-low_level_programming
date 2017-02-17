dd8b22acaaf04cd69729d025cb6b702bdd307460
Spencer Cheng <spcheng0412@gmail.com>
2017-02-17 06:43:10 +0000

    update

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * print_square: Function that prints our a square
+ * print_square - Function that prints our a square
  * followed by a new line.
- * size: arguement that represents length of square
+ * @size: arguement that represents length of square
  * Return: hashtags
  */
 
 void print_square(int size)
 {
 	int i, j;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
@@ -18,14 +18,14 @@ void print_square(int size)
 	else
 	{
 		for (i = 0; i < size; i++)
-		{	
-			for (j = 0; j < size; j++) 
+		{
+			for (j = 0; j < size; j++)
 			{
 			_putchar('#');
 			}
-	_putchar('\n');	
+	_putchar('\n');
 		}
-	
 	}
+}
 }
 

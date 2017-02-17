ed21390454094c5275b83afa59515094f1dfdbb5
Spencer Cheng <spcheng0412@gmail.com>
2017-02-17 06:32:03 +0000

    update

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers: function prints the numbers
+ * print_most_numbers - function prints the numbers
  * from 0 to 9 while omitting 2 and 4, followed by a new line using
  * _putchar only twice.
  * Return: void
@@ -13,7 +13,7 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if(i == 2 || i == 4)
+		if (i == 2 || i == 4)
 		{
 			continue;
 		}

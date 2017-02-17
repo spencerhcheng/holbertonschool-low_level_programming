a4523d06cc0f1fde81479f286c07ce0cd71f5271
Spencer Cheng <spcheng0412@gmail.com>
2017-02-17 06:37:35 +0000

    update

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_line: A function that prints a 
+ * print_line - A function that prints a
  * straight line in the terminal
  * @n: takes in number of times the _ char should be printed
  * Return: underscore character
@@ -12,13 +12,13 @@ void print_line(int n)
 	int i;
 	char s = '_';
 
-	for (i = 0; i < n; i++) 
-	{		
+	for (i = 0; i < n; i++)
+	{
 		if (n > 0)
-		 {
+		{
 		_putchar(s);
 		}
-	
+
 	}
 		_putchar('\n');
 }

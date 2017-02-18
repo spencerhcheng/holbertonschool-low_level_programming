68773a40192417bb83874bd4c6c5962fe329aa6f
Spencer Cheng <spcheng0412@gmail.com>
2017-02-18 19:53:45 +0000

    fixed missing semicolon in header file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,9 +16,9 @@ void print_to_98(int n)
 	}
 	while (n <= 97)
 	{
-	printf("%d, ", n);
-	n++;
+		printf("%d, ", n);
+		n++;
 	}
-	printf("98");
-	putchar('\n');
+		printf("98");
+		putchar('\n');
 }

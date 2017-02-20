c007bb35d919461e629568cd1068f316b267ed77
Spencer <spencerhcheng@gmail.com>
2017-02-20 07:58:05 +0000

    fixed spaces

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,50 +9,50 @@
  */
 void print_times_table(int n)
 {
-        int i, j, x;
+	int i, j, x;
 
-        for (i = 0; i <= n; i++)
-        {
-                if (n > 15 || n < 0)
-                { break; }
-                for (j = 0; j <= n; j++)
-                {
-                        x = i * j;
-                        if (j == 0)
-                        {
-                                _putchar(x % 10 + '0');
-                        }
-                        if (x <= 9 && j != 0)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar(' ');
-                                _putchar(' ');
-                                _putchar(x % 10 + '0');
-                                if (j == n)
-                                { break; }
-                                                }
-                        else if (x > 9 && x <= 99)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar(' ');
-                                _putchar(x / 10 + '0');
-                                _putchar(x % 10 + '0');
-                                if (j == n)
-                                { break; }
-                                                        }
-                        else if (x > 99)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar(x / 100 + '0');
-                                _putchar(x / 10 % 10  + '0');
-                                _putchar(x % 10 + '0');
-                                if (j == n)
-                                { break; }
-                        }
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i <= n; i++)
+	{
+		if (n > 15 || n < 0)
+		{ break; }
+			for (j = 0; j <= n; j++)
+			{
+				x = i * j;
+				if (j == 0)
+				{
+					_putchar(x % 10 + '0');
+				}
+					if (x <= 9 && j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(x % 10 + '0');
+					if (j == n)
+					{ break; }
+					}
+					else if (x > 9 && x <= 99)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(x / 10 + '0');
+						_putchar(x % 10 + '0');
+					if (j == n)
+					{ break; }
+					}
+					else if (x > 99)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(x / 100 + '0');
+						_putchar(x / 10 % 10  + '0');
+						_putchar(x % 10 + '0');
+					if (j == n)
+					{ break; }
+					}
+			}
+		_putchar('\n');
+	}
 }

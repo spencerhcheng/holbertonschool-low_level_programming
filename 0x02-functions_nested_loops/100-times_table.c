48e7cb9eb9a31ad8b4de373961f0bd0a8fc6c7b3
Spencer Cheng <spencercheng@Spencers-iMac.local>
2017-02-19 23:21:13 -0800

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,63 +1,58 @@
 #include "holberton.h"
 
 /**
- * times_table - prints the n times table
+ * print_times_table - prints the n times table
  * beginning with 0. If n is greater than 15
  * or less than 0, the function should not print anything.
+ * @n: integer n for mult table
  * Return: nothing
  */
-
 void print_times_table(int n)
 {
-	int i, j;
-	int x; 
+        int i, j, x;
 
-	for (i = 0; i <= n; i++) 
-	{
-		if (n > 15 || n < 0)
-		{
-			break;
-		}
-		for (j = 0; j <= n; j++)
-		{
-			x = i * j;
-			if (x <= 9 ) 
-			{
-				_putchar(x % 10 + '0');
-				if (j == n)
-				{
-					break;
-				}
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (x > 9 && x <= 99)
-			{
-				_putchar(x / 10 + '0');
-				_putchar(x % 10 + '0');
-				if (j == n)
-				{
-					break;
-				}
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else 
-			{
-				_putchar(x / 100 + '0');
-				_putchar(x / 10 % 10  + '0');
-				_putchar(x % 10 + '0');
-				if (j == n)
-				{
-					break;
-				}
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+        for (i = 0; i <= n; i++)
+        {
+                if (n > 15 || n < 0)
+                { break; }
+                for (j = 0; j <= n; j++)
+                {
+                        x = i * j;
+                        if (j == 0)
+                        {
+                                _putchar(x % 10 + '0');
+                        }
+                        if (x <= 9 && j != 0)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                _putchar(' ');
+                                _putchar(' ');
+                                _putchar(x % 10 + '0');
+                                if (j == n)
+                                { break; }
+                                                }
+                        else if (x > 9 && x <= 99)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                _putchar(' ');
+                                _putchar(x / 10 + '0');
+                                _putchar(x % 10 + '0');
+                                if (j == n)
+                                { break; }
+                                                        }
+                        else if (x > 99)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                _putchar(x / 100 + '0');
+                                _putchar(x / 10 % 10  + '0');
+                                _putchar(x % 10 + '0');
+                                if (j == n)
+                                { break; }
+                        }
+                }
+                _putchar('\n');
+        }
 }

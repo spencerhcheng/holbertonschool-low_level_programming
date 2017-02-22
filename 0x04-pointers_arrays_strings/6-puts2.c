7e98dbc2855713a2aa68633055b9ca3db90fd245
Spencer Cheng <spcheng0412@gmail.com>
2017-02-22 05:20:07 +0000

    fixed spacing

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 
-/** 
- * _puts - A function that prints a string, followed by a new line to standard out
+/**
+ * _puts - A function that prints a string,
+ * followed by a new line to standard out
  * @str: string as arguement
  * Return: nothing
  */
 
 void puts2(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i] != '\0')
-        {
-                _putchar(str[i]);
-                i = i + 2;
-        }
-                _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+		_putchar('\n');
 }

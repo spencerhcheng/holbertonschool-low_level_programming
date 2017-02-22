289e3405f1e80a51580716b1f6a19c939c4b63f3
Spencer Cheng <spcheng0412@gmail.com>
2017-02-22 07:37:53 +0000

    fixed for edge cases

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,17 @@ void print_array(int *a, int n)
 
 	int i;
 
+	if (n > 0)
+	{
+
 	for (i = 0; i < n - 1 ; i++)
 	{
 		printf("%i, ", a[i]);
 	}
 		printf("%i\n", a[n - 1]);
+	}
+	else
+	{
+		printf("\n");
+	}
 }

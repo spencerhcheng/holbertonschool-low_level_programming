dfa792b7de7bba3836ccf2643befc851fb1d262c
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-02-23 20:00:45 +0000

    upload files

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+char *_strcat(char *dest, char *src)
+{
+	char val;
+
+	int i, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		val[i] = dest[i];
+		i++;
+	}
+	
+	while (src[j] != '\0')
+	{
+		val[j] = src[j];
+		i++;
+	}
+	return (val);
+}
+		

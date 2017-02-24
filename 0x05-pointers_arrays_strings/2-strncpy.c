196c0414a5b10daca74c96fde04ddc23d4d4030d
Spencer <spencerhcheng@gmail.com>
2017-02-24 00:47:19 +0000

    original

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - concatenates two strings
+ * up to n bytes of src
+ * @dest: string arguement
+ * @src: string arguement
+ * @n: integer value of bytes to pring src
+ * Return: final string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	for ( ; j < n; j++)
+	{
+		dest[j] = '\0';
+	}
+	return (dest);
+}

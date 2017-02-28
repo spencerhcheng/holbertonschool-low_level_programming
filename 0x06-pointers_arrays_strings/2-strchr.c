666a0656db2490fa96124973fc600a5dc7d06bb7
Spencer Cheng <spencerhcheng@gmail.com>
2017-02-28 00:20:44 +0000

    update

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			break;
+			return (&s[i]);
 		}
 	}
-	return (&s[i]);
+	return ('\0');
 }

8f70e72aeed9fa53c00403b13d43754f826ea5bb
Spencer Cheng <spencerhcheng@gmail.com>
2017-02-28 02:32:29 +0000

    changed conditional

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	int counter = 0;
 
-	for (i = 0; s[i] != ' '; i++)
+	for (i = 0; s[i] > ' '; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{

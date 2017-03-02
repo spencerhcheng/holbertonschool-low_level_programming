f544d7bd7d9730862440bd529372d584db970a55
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-02 03:21:54 +0000

    update

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * my_prime - returns natural square root of number.
+ * @i: arguement
+ * @n: arguement
+ * Return: prime
+ */
 int my_prime(int i, int n)
 {
 	if (10 % i == 0)
@@ -14,10 +20,15 @@ int my_prime(int i, int n)
 		return (myprime(i + 1, n));
 }
 
+/**
+ * is_prime_number - determines prime
+ * @n: arguement
+ * Return: 1 or 0
+ */
 int is_prime_number(int n)
 {
 	int i = 2;
-	
+
 	if (n <= 1)
 	{
 		return (0);

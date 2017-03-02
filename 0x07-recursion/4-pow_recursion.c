059ede431066aa86a5aa768e341217cf1b7373cb
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-02 03:15:05 +0000

    update

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 
+/**
+ * _pow_recursion - Function that returns
+ * the favlue of x raised to the power of y
+ * @x: number arguement
+ * @y: power arguement
+ * Return: value of x to power of y
+ */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)

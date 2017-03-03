66ebf2dc1bde9793a2ff91477989dde5fe847f50
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-03 21:49:18 +0000

    fix spacing

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,18 +1,32 @@
 #include "holberton.h"
 
+/**
+ * rec_root - Function that returns
+ * the natural square root of a number.
+ * @i: variable i
+ * @n: number arguement
+ * Return: function
+ */
+
 int rec_root(int i, int n)
 {
 	if ((i * i == n))
 	{
-		return(i);
+		return (i);
 	}
-	else if (i * i > n) 
+	else if (i * i > n)
 	{
 		return (-1);
 	}
 	else
-		return(rec_root(i + 1, n));
+		return (rec_root(i + 1, n));
 }
+
+/**
+ * _sqrt_recursion - function to pass in i
+ * @n: number arguement
+ * Return: number
+ */
 
 int _sqrt_recursion(int n)
 {

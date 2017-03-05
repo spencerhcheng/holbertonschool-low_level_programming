2c0c1038952fde732a370cfafa6ba205956a1e8e
Spencer <spencerhcheng@gmail.com>
2017-03-05 03:05:17 +0000

    new update 4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) >= 0 && atoi(argv[i]) <= INT_MAX)
+			if (atoi(argv[i]) > 0 && atoi(argv[i]) <= INT_MAX)
 			{
 				sum += atoi(argv[i]);
 			}

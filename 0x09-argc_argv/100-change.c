82535914bf9f41447922f7c40bfe2f6cb3b2c276
Spencer <spencerhcheng@gmail.com>
2017-03-07 05:05:14 +0000

    first commit

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - takes in number amount of pennies  and
+ * returns minimum number of coins to make change
+ * @argc: number of arguements
+ * @argv: arguements in penny amount
+ * Return: coin coint
+ */
 int main(int argc, char *argv[])
 {
-	int i;
-	int j;
-	int count;
-	int newTotal;
+	int count, newTotal;
 
-	j = 1;
 	count = 0;
-	
-//	if (argc != 1)
-//	{
-//		printf("Error\n");
-//		return (1);
-//	}
+	newTotal = atoi(argv[1]);
 
-	i = atoi(argv[j]);
-	newTotal = i;
+	if (argc < 0)
+		printf("0\n");
 
-	while (newTotal > 0)
+	else if (argc != 2)
 	{
-		if(newTotal > 25)
-		{
-			newTotal -= 25;
-			continue;
-		}
-		else if (newTotal >= 10 && newTotal < 25)
-		{
-			newTotal -= 10;
-		}
-		else if (newTotal >= 5 && newTotal < 10)
-		{
-			newTotal -= 5;
-		}
-		else if (newTotal >= 2 && newTotal < 5)
-		{
-			newTotal -= 2;
-		}
-		else
-		{
-			newTotal -=1;
-		}
-			count += 1;
+		printf("Error\n");
+		return (1);
 	}
-	printf("%d\n", count);
+	else
+	{
+		while (newTotal > 0)
+		{
+			if (newTotal >= 25)
+				newTotal -= 25;
+
+			else if (newTotal >= 10 && newTotal < 25)
+				newTotal -= 10;
+
+			else if (newTotal >= 5 && newTotal < 10)
+				newTotal -= 5;
+
+			else if (newTotal >= 2 && newTotal < 5)
+				newTotal -= 2;
+			else
+			{
+				newTotal -= 1;
+			}
+				count += 1;
+		}
+	}
+		printf("%d\n", count);
 	return (0);
 }

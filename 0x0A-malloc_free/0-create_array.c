a60ee234e201de84d60b327d44d4d89fc72482e1
Spencer <spencerhcheng@gmail.com>
2017-03-08 00:30:19 +0000

    added comments and removed trailing whitespace

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * *create_array - a function that creates
+ * an array of chars, and initializes it
+ * with a specific char
+ * @size: length of string
+ * @c: character arguement
+ * Return: pointer to array
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *ar;
-	unsigned int i;	
+	unsigned int i;
 
 	ar = malloc(size * sizeof(char));
-	
+
 	i = 0;
-	
+
 	if (size == 0)
 	{
 		return (NULL);

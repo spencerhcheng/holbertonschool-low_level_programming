96d5d15231cd2d2ffde48927a8b706651c812fc7
Spencer Cheng <spcheng0412@gmail.com>
2017-03-08 08:28:02 +0000

    removed whitespace and added comments

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * free_grid - frees up memory from grid
+ * @grid: grid arguement
+ * @height: height of grid
+ * Return: nothing
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i;
-	
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);

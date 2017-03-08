981d8bc53e38fe3e1e0e210afe56f11d44111d35
Spencer <spencerhcheng@gmail.com>
2017-03-08 22:37:52 +0000

    freeing mem

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -13,16 +13,12 @@ int **alloc_grid(int width, int height)
 	int **ar;
 	int i, j;
 
-	if (width <= 0)
-	{
-		return (NULL);
-	}
-	if (height <= 0)
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	ar = malloc(height * sizeof(int *));
+	ar = malloc(height * sizeof(ar));
 
 	if (ar == NULL)
 	{
@@ -30,14 +26,25 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-		for (i = 0; i < height; i++)
-		{
-			ar[i] = malloc(width * sizeof(int));
+	for (i = 0; i < height; i++)
+	{
+		ar[i] = malloc(width * sizeof(int));
 
+		if (ar[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(ar[j]);
+				{
+					free(ar);
+					return (NULL);
+				}
+			}
+		}
 		for (j = 0; j < width; j++)
 		{
 			ar[i][j] = 0;
 		}
 	}
-	return (ar);
+		return (ar);
 }

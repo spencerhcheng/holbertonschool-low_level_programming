eb1a1aec3855a1e0c6fb1a3e40b7018b836699ab
Spencer Cheng <spcheng0412@gmail.com>
2017-03-08 06:33:51 +0000

    update #2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,18 +10,31 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *combined;
-
+	char *combo;
+	int i;
+	int j;
+	
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
 
-	combined = malloc((strlen(s1) + strlen(s2)) * sizeof(char));
+	combo = malloc((strlen(s1) + strlen(s2)) * sizeof(char));
 
-	strcat(combined, s1);
-	strcat(combined, s2);
-
-	return (combined);
+	if (combo == NULL)
+	{
+		return (NULL);
+	}
+	
+	for (i = 0; s1[i] != '\0'; i++)
+	{	
+		combo[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; i++, j++)
+	{
+		combo[i] = s2[j];
+	}
+	combo[i] = '\0';
+	return (combo);
 }

ab65112a72c176b593f214fd1db04ac3ae2136fd
Spencer <spencerhcheng@gmail.com>
2017-03-09 02:51:04 +0000

    update 7

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -12,12 +12,12 @@ int **alloc_grid(int width, int height)
 	int  **ar;
 	int i, j;
 
+	ar = malloc((sizeof(int *) * height));
+	
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-
-	ar = malloc((sizeof(int *) * height));
 
 	if (ar == NULL)
 	{

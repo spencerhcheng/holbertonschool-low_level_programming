3643047730e29e663480e8e0e1ad270e3b720dc5
Spencer <spencerhcheng@gmail.com>
2017-03-09 21:39:58 +0000

    original

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - function concats two strings
+ * s1 and s2 up to n bytes
+ * @s1: pointer to string1
+ * @s2: pointer to string2
+ * @n: number of n bytes
+ * Return: string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *combo;
+	unsigned int i, j;
+	unsigned int len1, len2;
+
+	len2 = n;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{
+	}
+
+	combo = malloc((len1 + len2) * sizeof(char));
+
+	if (combo == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		combo[i] = s1[i];
+	}
+	for (j = 0; j < n; j++, i++)
+	{
+		combo[i] = s2[j];
+	}
+	return (combo);
+}

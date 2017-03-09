ddbe2dbc68fabe3f12567a92fa2ad6c098dd5827
Spencer <spencerhcheng@gmail.com>
2017-03-09 21:45:58 +0000

    added condition if string is greater than n

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	unsigned int len1, len2;
 
-	len2 = n;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -24,21 +23,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
-	{
-	}
+	;
+
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	;
+
+	if (len2 >  n)
+		len2 = n;
 
 	combo = malloc((len1 + len2) * sizeof(char));
 
 	if (combo == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		combo[i] = s1[i];
 	}
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j < len2; j++, i++)
 	{
 		combo[i] = s2[j];
 	}

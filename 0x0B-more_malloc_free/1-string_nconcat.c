c79c4629cb30dfc2f13d330939ec7b525c39c7ed
Spencer <spencerhcheng@gmail.com>
2017-03-09 21:54:24 +0000

    update

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	unsigned int len1, len2;
 
-
 	if (s1 == NULL)
 		s1 = "";
 
@@ -37,13 +36,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		combo[i] = s1[i];
-	}
+
 	for (j = 0; j < len2; j++, i++)
-	{
 		combo[i] = s2[j];
-	}
+
 	combo[i] = '\0';
 	return (combo);
 }

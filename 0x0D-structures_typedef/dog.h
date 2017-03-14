dd5f3e3a16c03aaecffbec0cb4eb7c67381fd9d2
Spencer <spencerhcheng@gmail.com>
2017-03-14 21:22:51 +0000

    added header prototypes

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -20,4 +20,6 @@ dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
+int _strlength(char *s);
+char *_strcpy(char *dest, char *src);
 #endif

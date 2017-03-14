4c914fda6ea73d18291974eaad45e97a82ba416a
Spencer <spencerhcheng@gmail.com>
2017-03-14 06:17:36 +0000

    remove uncessary

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer to dog
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d->name != NULL)
+	{
+		free(d->name);
+	}
+
+	if (d->owner != NULL)
+	{
+		free(d->owner);
+	}
+
+	free(d);
+}

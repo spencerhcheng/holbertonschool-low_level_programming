81ec22883024eb3a5478b738def92d204ed7e171
Spencer <spencerhcheng@gmail.com>
2017-03-14 06:46:15 +0000

    fixed and shortened

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -4,18 +4,52 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, j;
-	dog_t *pup;
+	dog_t *newDawg;
 
 	if (name == NULL || owner == NULL)
-	{
 		return (NULL);
-	}
-	
-	pup = malloc(sizeof(dog_t);
-	
+
+	newDawg = malloc(sizeof(dog_t));
+
 	if (name != NULL)
 	{
 		for (i = 0; name[i] != '\0'; i++)
 		;
 
-		pup->name = 
+		newDawg->name = malloc(i * sizeof(char));
+
+		if (newDawg->name == NULL)
+		{
+			free(newDawg->name);
+			return (NULL);
+		}
+
+		for (i = 0; name[i] != '\0'; i++)
+			newDawg->name[i] = name[i];
+	}
+	else
+		return (NULL);
+
+	if (owner != NULL)
+	{
+		for (j = 0; owner[j] != '\0'; j++)
+		;
+
+		newDawg->owner = malloc(j * sizeof(char));
+
+		if (newDawg->owner == NULL)
+		{
+			free(newDawg->owner);
+			return (NULL);
+		}
+
+		for (j = 0; owner[j] != '\0'; j++)
+			newDawg->owner[j] = owner[j];
+	}
+	else
+		return (NULL);
+
+	newDawg->age = age;
+
+	return (newDawg);
+}

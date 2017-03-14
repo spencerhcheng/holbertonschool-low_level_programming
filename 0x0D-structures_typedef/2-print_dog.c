150885fc8e1dba4a2be007fb7ff2f29e9ed81dac
Spencer <spencerhcheng@gmail.com>
2017-03-14 04:13:48 +0000

    fixed positioning

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -17,10 +17,10 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
+	printf("Age: %f\n", d->age);
+
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
-
-	printf("Age: %f\n", d->age);
 }

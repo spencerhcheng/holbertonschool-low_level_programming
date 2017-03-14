a8630def957096a8378257f2b287af581a4e601a
Spencer <spencerhcheng@gmail.com>
2017-03-14 21:20:14 +0000

    added helpers

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,63 +1,91 @@
 #include "dog.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * new_dog - creates new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: dog
+ * _strlength - function that returns the length of a string
+ * @s: string that is input
+ * Return: length
  */
 
+int _strlength(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	;
+	i++;
+	return (i);
+}
+
+/**
+ * *_strcpy - function that copies the string pointed to by src,
+ * including the terminating null byte \0 to the buffer pointed
+ * to by dest.
+ * @dest: pointer to buffer
+ * @src: pointer to string
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - create a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: new dog struct
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, j;
-	dog_t *newDawg;
+	dog_t *fido;
+	int len1, len2, i, j;
+
+	len1 = _strlength(name);
+	len2 = _strlength(owner);
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	newDawg = malloc(sizeof(dog_t));
+	fido = malloc(sizeof(dog_t));
 
-	if (name != NULL)
+	if (fido == NULL)
 	{
-		for (i = 0; name[i] != '\0'; i++)
-		;
-
-		newDawg->name = malloc(i * sizeof(char));
-
-		if (newDawg->name == NULL)
-		{
-			free(newDawg->name);
-			return (NULL);
-		}
-
-		for (i = 0; name[i] != '\0'; i++)
-			newDawg->name[i] = name[i];
-	}
-	else
+		free(fido);
 		return (NULL);
+	}
 
-	if (owner != NULL)
+	fido->name = malloc(sizeof(char) * len1);
+
+	if (fido->name == NULL)
 	{
-		for (j = 0; owner[j] != '\0'; j++)
-		;
-
-		newDawg->owner = malloc(j * sizeof(char));
-
-		if (newDawg->owner == NULL)
-		{
-			free(newDawg->owner);
-			return (NULL);
-		}
-
-		for (j = 0; owner[j] != '\0'; j++)
-			newDawg->owner[j] = owner[j];
-	}
-	else
+		free(fido->name);
 		return (NULL);
+	}
 
-	newDawg->age = age;
+	*_strcpy(fido->name, name);
 
-	return (newDawg);
+	fido->owner = malloc(len2 * sizeof(char));
+
+	if (fido->owner == NULL)
+	{
+		free(fido->name);
+		free(fido->owner);
+		return (NULL);
+	}
+
+	*_strcpy(fido->owner, owner);
+
+	fido->age = age;
+	return (fido);
 }

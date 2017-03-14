76dad1516fef3a87ef7356ab4bd8cf3622e7266b
Spencer <spencerhcheng@gmail.com>
2017-03-14 03:46:43 +0000

    seperated conditions

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -9,11 +9,20 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL || d->age == NULL || d->owner == NULL)
+	if (d->name == NULL)
 	{
 		d->name = "(nil)";
 	}
 
+	if (d == NULL)
+	{
+		return;
+	}
+	
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";	
+	}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);

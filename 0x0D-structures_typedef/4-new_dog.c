009652c98a1905f7129af129b920278e21e3cbd7
Spencer <spencerhcheng@gmail.com>
2017-03-14 06:48:03 +0000

    update

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * new_dog - creates new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: dog
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, j;

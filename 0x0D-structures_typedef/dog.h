050b15518f3054293a5e0e164da604ecf92c9e11
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-14 01:54:43 +0000

    added comment

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,9 +1,20 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+/**
+ * struct dog - define new type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: Jay's dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif

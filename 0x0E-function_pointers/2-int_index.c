fbee19db0f5f0c5a83112adb8ae274c1ffe6d455
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-15 21:53:01 +0000

    original

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * int_index - function that searches for an index
+ * @array: pointer to array
+ * @size: pointer to array size
+ * @cmp: pointer to function used to compare values
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int result;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		result = (cmp)(array[i]);
+
+		if (result != 0)
+		{
+			return (i);
+		}
+		if (result != 0 && result != -1)
+		{
+			return (-1);
+		}
+	}
+}

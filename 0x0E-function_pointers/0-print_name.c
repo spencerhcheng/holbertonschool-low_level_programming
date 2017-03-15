2c7797a3d2ed7547c115a642f4b38865ee613faa
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-15 19:52:18 +0000

    added if name is NULL

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - A function that prints a name
@@ -11,10 +12,7 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
 	{
-		return (NULL);
+		return;
 	}
-	else
-	{
 	f(name);
-	}
 }

13db971609c493488e06c94d0b4b08ea01ac957e
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-15 20:08:53 +0000

    update

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
+#ifndef FUNCPOINT_H
+#define FUNCPOINT_H
 #include <stddef.h>
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif

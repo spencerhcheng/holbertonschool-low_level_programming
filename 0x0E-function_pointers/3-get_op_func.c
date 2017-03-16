1ecdb48361e4aaaf9bae6d6314f2c473005d3195
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-16 22:15:15 +0000

    changed condition in while loop with a string compare

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - function that selects the correct
@@ -26,7 +27,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (ops[i].op[0] == s[0])
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
 		}

77f7b19198e89e00f6146e920823cff49e5b7f21
Spencer Cheng <spencer.cheng@holbertonschool.com>
2017-03-16 06:00:19 +0000

    update

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -26,9 +26,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (strcmp(ops[i]->op, s) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 		{
-			return (ops[i]->f);
+			return (ops[i].f);
 		}
 		i++;
 	}

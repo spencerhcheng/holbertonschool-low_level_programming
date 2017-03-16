dbca2b3a8faa93c58561e7c7bf171008dc00fedf
Spencer <spencerhcheng@gmail.com>
2017-03-16 21:17:47 +0000

    fixed spacing in front of lines

--- 0x0E-function_pointers/3-calc.h
+++ 0x0E-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef OPERATOR_H
-#define OPERATOR_H
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
 /**
  * struct op - Struct op
  *
@@ -12,6 +12,7 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);

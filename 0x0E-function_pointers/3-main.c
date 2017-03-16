425e168a4d2bf0a5d11905a7ce64182f66ff5e10
Spencer <spencerhcheng@gmail.com>
2017-03-16 21:23:00 +0000

    removed whitespace and added comment section

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,23 +1,30 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc,char *argv[])
+/**
+ * main - function converts string to numbers
+ * and prints out result of number math operators
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
-	int x, y;
+	int num1, num2;
 	int op;
 
- 	if (argc != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);	
+		exit(98);
 	}
-		
-	x = atoi(argv[1]);
-	y = atoi(argv[3]);
 
-	op = (*get_op_func(argv[2]))(x, y);
-	
-	printf("%d\n", op);	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	op = (*get_op_func(argv[2]))(num1, num2);
+
+	printf("%d\n", op);
 	return (0);
 }

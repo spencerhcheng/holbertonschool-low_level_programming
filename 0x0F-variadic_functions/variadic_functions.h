7c06d2530befe3d29599dd11663da3f5eda13256
Spencer <spencerhcheng@gmail.com>
2017-03-19 22:13:36 +0000

    updated struct variables

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,18 +1,25 @@
-#ifndef VARIADIC_H
-#define VARIADIC_H
-typedef struct variad
+#ifndef VARIAD_H
+#define VARIAD_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct prints - structure that takes in a type
+ * @type: argument type: char, int, float or string
+ * @f: function
+ * and a function
+ */
+typedef struct prints
 {
-	char *variad;
-	char (*f)();
-}argType;
-
+	char *type;
+	void (*f)();
+} prints;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(char);
-void print_int(int);
-void print_float(float);
-void print_string(char);
-void print_all(const char * const format, ...);
+void func_char(char *separator, va_list args);
+void func_int(char *separator, va_list args);
+void func_float(char *separator, va_list args);
+void func_string(char *separator, va_list args);
 #endif

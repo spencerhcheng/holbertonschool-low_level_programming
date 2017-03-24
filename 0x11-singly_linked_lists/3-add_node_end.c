5402a1fd4fb72f6b9a1778e810a120f6dfc3bef7
Spencer <spencerhcheng@gmail.com>
2017-03-24 05:04:03 +0000

    added case for if temp == NULL

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - function that adds a new node at the
+ * end of a list_t list.
+ * @head: double pointer to head
+ * @str: pointer to string
+ * Return: address of new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	unsigned int count;
+	list_t *new;
+	list_t *temp;
+
+	count = 0;
+	temp = *head;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	new->str = strdup(str);
+	new->len = count;
+	new->next = NULL;
+
+	if (*head != NULL)
+	{
+		temp = *head;
+		while (new->next != NULL)
+		{
+			new = new->next;
+		}
+		temp->next = new;
+		temp = new->next;
+	}
+
+	else
+	{
+		*head = new;
+		return (new);
+	}
+	return (temp);
+}

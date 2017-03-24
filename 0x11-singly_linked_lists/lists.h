0e5ee1bd77f9a0e8509a47611a770a513837af95
Spencer <spencerhcheng@gmail.com>
2017-03-24 06:11:13 +0000

    fixed trailing whitespace issues and fixed long unsigned int to unsigned long int

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -11,12 +11,12 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-typedef long unsigned int size_t;
+typedef unsigned long int size_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);

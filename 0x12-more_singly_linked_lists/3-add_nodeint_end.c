3524fb9f476455d92d1ecf942759d3ee0dfa2ab6
Spencer <spencerhcheng@gmail.com>
2017-03-28 18:10:24 +0000

    removed trailing whitespaces on lines 17, 21 and 26

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,16 +14,16 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *tmp;
 	listint_t *new;
-	
+
 	tmp = *head;
 
 	new = malloc(sizeof(listint_t));
-	
+
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	new->n = n;
 	new->next = NULL;
 

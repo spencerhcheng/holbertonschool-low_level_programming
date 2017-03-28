6f8880cbebcd69712228ffeb83fed9ed640266ca
Spencer <spencerhcheng@gmail.com>
2017-03-28 18:18:32 +0000

    add lines around = on line 21

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -12,9 +12,12 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
+	temp = head;
+
 	while (head != NULL)
 	{
-		temp = head;
+		head = head->next;
 		free(temp);
+		temp = temp->next;
 	}
 }

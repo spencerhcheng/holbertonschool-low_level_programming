d2b6c2e8f38844e2631deb19aec1d2c53bbc9471
Spencer <spencerhcheng@gmail.com>
2017-03-29 07:16:17 +0000

    simplified to tmpNext->next->next

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,8 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		tmp = tmp->next;
 	}
 
-	tmpNext = tmp->next;
-	tmp->next = tmpNext->next;
+	tmpNext = tmp->next->next;
 	free(tmpNext);
 	return (1);
 }

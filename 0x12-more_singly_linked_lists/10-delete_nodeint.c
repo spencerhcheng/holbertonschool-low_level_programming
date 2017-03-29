9b155ff55269360662f14fdbcbe5b747331a30c3
Spencer <spencerhcheng@gmail.com>
2017-03-29 17:21:01 +0000

    changed tmpNext = tmp->next to tmp->next = tmpNext->next

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	tmpNext = tmp->next;
-	tmpNext = tmpNext->next;
+	tmp->next = tmpNext->next;
 	free(tmpNext);
 	return (1);
 }

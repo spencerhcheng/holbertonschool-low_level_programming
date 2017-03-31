58e87162f2b4c405937b25d8d9b08958fbffdb1a
Spencer <spencerhcheng@gmail.com>
2017-03-31 06:04:43 +0000

    fixed shift operator

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int y, i;
+	char is_leading_zero = 1;
+
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		y = (n << 1);
+		y = (y >> 1);
+
+		if (n != y) /* left most 1 */
+		{	
+			is_leading_zero = 0;
+			_putchar('1');
+		}
+		else if ((is_leading_zero != 1))
+		{
+			_putchar('0');
+		}
+			n = n << 1;
+	}
+	if (is_leading_zero == 1)
+		_putchar('0');
+}

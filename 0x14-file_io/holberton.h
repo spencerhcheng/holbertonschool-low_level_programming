caf0cf18ff9c0b28ba705af8d27db6517c82e03c
Spencer <spencerhcheng@gmail.com>
2017-04-04 21:05:06 +0000

    updated readme and adding header file

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -2,8 +2,10 @@
 #define FILEIO_H
 #include <stdio.h>
 #include <sys/types.h>
-#include <font1.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

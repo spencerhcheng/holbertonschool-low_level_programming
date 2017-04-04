926d763755c7bd551714015df9c5c0c42607eb81
Spencer <spencerhcheng@gmail.com>
2017-04-04 17:29:45 +0000

    add headers and prototypes into header file

---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,11 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+#include <stdio.h>
+#include <sys/types.h>
+#include <font1.h>
+#include <unistd.h>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif

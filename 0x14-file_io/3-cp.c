ba69b1d8f27e48eb4edf41eac1461dcba45a1f56
Spencer <spencerhcheng@gmail.com>
2017-04-05 23:26:58 +0000

    fixed error comments

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	char buff[1204];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to", argv[1]), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)

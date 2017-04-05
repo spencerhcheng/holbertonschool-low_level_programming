a859d82d03934c59978bee29f7ed3c78cb95cc85
Spencer <spencerhcheng@gmail.com>
2017-04-05 02:43:04 +0000

    fixed error check on cd

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -32,9 +32,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		wd = write(fd, '\0', count);
 
-	close(fd);
+	cd = close(fd);
 
-	if (close == -1)
+	if (cd == -1)
 		return (-1);
 
 	if (wd == -1)

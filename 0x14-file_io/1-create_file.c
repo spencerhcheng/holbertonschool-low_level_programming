010ff4eadf1b34f9bd75cde11853b5a660c24bce
Spencer <spencerhcheng@gmail.com>
2017-04-05 00:25:48 +0000

    changed permissions

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	else
 	{
-		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 
 		if (fd == -1)
 			return (-1);

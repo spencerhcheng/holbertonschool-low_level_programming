8b3dea09b7318fdfd0402bf1973c8396d3877ff2
Spencer <spencerhcheng@gmail.com>
2017-04-05 22:35:26 +0000

    moved counter loop after check if text is null

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -15,16 +15,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-		fd = open(filename, O_WRONLY | O_APPEND);
+	if (text_content == NULL)
+		return (1);
 
-		if (fd == -1)
-			return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
 
 	for (count = 0; text_content[count] != '\0'; count++)
 		;
-
-	if (text_content == NULL)
-		return (1);
 
 	wd = write(fd, text_content, count);
 

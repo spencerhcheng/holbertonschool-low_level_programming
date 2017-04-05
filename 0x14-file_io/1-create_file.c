7a07d7ade8215c7613c8ab06004c68d563e2df66
Spencer <spencerhcheng@gmail.com>
2017-04-05 22:20:30 +0000

    removed if else

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,13 +14,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	else
-	{
-		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 
-		if (fd == -1)
-			return (-1);
-	}
+	if (fd == -1)
+		return (-1);
 
 	for (count = 0; text_content[count]; count++)
 		;

2ed3bff649562cd8fb6eaa932ebf5e7d2f08d071
Spencer <spencerhcheng@gmail.com>
2017-04-05 22:24:18 +0000

    changed permission to numeric, moved error check for write, count until null byte

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,23 +14,23 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	for (count = 0; text_content[count]; count++)
-		;
-
 	if (text_content == NULL)
 		text_content = "";
 
-	wd = write(fd, text_content, count);
+	for (count = 0; text_content[count] != '\0'; count++)
+		;
 
-	close(fd);
+	wd = write(fd, text_content, count);
 
 	if (wd == -1)
 		return (-1);
+
+	close(fd);
 
 	return (1);
 }

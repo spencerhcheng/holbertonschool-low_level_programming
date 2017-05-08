b8076160d23a1cb2d0fa409fa691aa0f1a8ebdac
Spencer <spencerhcheng@gmail.com>
2017-05-08 23:40:54 +0000

    7-insert_dnode: revision

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,52 +1,40 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - a function that
- * inserts a new node at a given position in
- * a linked list
- * @h: pointer to struct node
- * @idx: index to insert node
- * @n: value to store in node
+ * insert_dnodeint_at_index - a function that inserts
+ * a new node at a given position
+ * @h: double pointer to head
+ * @idx: index position of node. initialized to 0.
+ * @n: value to insert into new node
  * Return: address of new node, or NULL if failed
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp;
-	dlistint_t *new;
-	size_t i, j;
+	dlistint_t *tmp, *new;
+	size_t i;
 
-	i = 0;
-	j = 0;
 	tmp = *h;
 
 	new = malloc(sizeof(dlistint_t));
 
-	if (new == NULL)
-		return (NULL);
+	new->n = n;
 
-	while (tmp != NULL)
-	{
-		tmp = tmp->next;
-		i++;
-	}
-
-	if (idx > i)
-		return (NULL);
-
-	else if (idx == 0)
+	if (idx == 0)
 	{
 		new->next = *h;
 		*h = new;
-		return(*h);
+		return (*h);
 	}
 
-	
-	while (j == idx - 1)
+	for (i = 0; tmp != NULL; tmp = tmp->next, i++)
 	{
-		new->next = tmp->next;
-		new->n = n;
-		tmp->next = new;
+		if (i == idx - 1)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
 	}
-		return (new);
+		return (NULL);
 }

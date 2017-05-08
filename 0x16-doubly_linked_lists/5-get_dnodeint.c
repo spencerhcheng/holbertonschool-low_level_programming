22fc0cc688dfd9df32a47c9ed91c8f547ac07215
Spencer <spencerhcheng@gmail.com>
2017-05-08 04:04:32 +0000

    5-get_dnodeint.c: initial commit

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function that returns
+ * the nth node of a dlistint_t linked list
+ * @head: pointer to struct node
+ * @index: nth node
+ * Return: node at index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+	size_t i, j;
+
+	i = 0;
+	j = 0;
+	tmp = head;
+
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	
+	if (index > i)
+		return (NULL);
+
+	while (head->next != NULL && j < index)
+	{
+		head = head->next;
+		j++;
+	}
+		return (head);
+}
+	

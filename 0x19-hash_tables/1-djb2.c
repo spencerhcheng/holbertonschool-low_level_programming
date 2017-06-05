8eb867cad9292351a7f1bed42fef4851969c8b4a
Spencer <spencerhcheng@gmail.com>
2017-06-05 17:23:34 +0000

    Add comment and resolve trailing whitespace

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,12 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hashing algorithm for strings by Dan Bernstein
+ * @str: string to hash using djb2 algorithm
+ * Return: hashed value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+		/* hash * 33 + c */
+	}
+return (hash);
 }

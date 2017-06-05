66acb9b7cf0abacf28029ef55a8b7edfa97103dc
Spencer <spencerhcheng@gmail.com>
2017-06-05 15:46:52 +0000

    Include comments and remove trailing whitespace

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,9 +1,15 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - creates a hash table
+ * where size is the size of the array.
+ * @size: size of the array in the hash table
+ * Return: pointer to the newly created hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *head;
-	
+
 	head = NULL;
 
 	head = malloc(sizeof(hash_table_t));
@@ -18,4 +24,4 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	return (head);
-} 
+}

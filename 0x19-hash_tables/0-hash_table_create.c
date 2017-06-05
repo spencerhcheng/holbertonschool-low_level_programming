db45f3c1c8646618224e0e942b52a08cd0a80689
Spencer <spencerhcheng@gmail.com>
2017-06-05 17:17:29 +0000

    Betty fixes to remove trailing whitespace

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,19 +9,25 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *head;
+	int i = 0;
 
 	head = NULL;
-
 	head = malloc(sizeof(hash_table_t));
 
 	if (head == NULL)
 		return (NULL);
 
 	head->size = size;
-	head->array = malloc(sizeof(hash_node_t) * size);
+	head->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (head->array == NULL)
 		return (NULL);
+
+	while (i < size)
+	{
+		head->array[i] = NULL;
+		i++;
+	}
 
 	return (head);
 }

ac67024ba866fe0e01f4d40a2e5584b8b5ac0c32
Spencer <spencerhcheng@gmail.com>
2017-06-06 01:41:56 +0000

    Remove curly braces

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -22,10 +22,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
-		{
 			return (temp->value);
-		}
 	}
-
-	return (NULL);
+		return (NULL);
 }

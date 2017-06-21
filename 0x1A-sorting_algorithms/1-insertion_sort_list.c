5cda11d4f58ac44820f089a547377bebddf419ae
Spencer <spencerhcheng@gmail.com>
2017-06-21 07:19:42 +0000

    1-insertion_sort_list: first working commit

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,45 +1,53 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order 
- * using the insertion sort algorithm. The list is printed every time elements are swapped.
+ * insertion_sort_list - sorts a doubly
+ * linked list of integers in ascending order
+ * using the insertion sort algorithm. The list
+ * is printed every time elements are swapped.
  * @list: double pointer to doubly linked listint_t list.
  * Return: void
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *prev_node, *current_node, *temp_prev, *temp_current;
+	listint_t *prev_node, *current_node;
 
 	prev_node = *list;
 	current_node = prev_node->next;
-	temp_prev = prev_node;
-	temp_current = current_node;
+	short int swap_flag = 0;
 
 	while (current_node != NULL)
 	{
-		prev_node = temp_prev;
-		current_node = temp_current;
-		/*
-		if (prev_node->n > current_node->n)
+		if (swap_flag == 1)
 		{
-			temp_prev = prev_node;
-			temp_current = current_node;
-			while (prev_node != NULL)
-			{
+			prev_node = *list;
+			current_node = prev_node->next;
+		}
+		swap_flag = 0;
+		while (current_node->prev != NULL && (prev_node->n > current_node->n))
+		{
+				swap_flag = 1;
+
+				if (prev_node->prev != NULL)
+				{
+					prev_node->prev->next = current_node;
+				}
 				prev_node->next = current_node->next;
-				current_node->prev = current_node;
+				current_node->prev = prev_node->prev;
+				if (current_node->next != NULL)
+					current_node->next->prev = prev_node;
 				prev_node->prev = current_node;
 				current_node->next = prev_node;
-				*list = current_node;
-				prev_node = prev_node->prev;
-			}
+				if (current_node->prev == NULL)
+				{
+					*list = current_node;
+					break;
+				}
+			print_list(*list);
+			prev_node = current_node->prev;
 		}
-		*/
-	
-	print_list(*list);
-	current_node = current_node->next;
-	prev_node = prev_node->next;
+		current_node = current_node->next;
+		prev_node = prev_node->next;
 	}
-	print_list(*list);
 }

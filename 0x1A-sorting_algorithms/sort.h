c14cc05d18a585334360e5901b0045e79cc8cf2f
Spencer <spencerhcheng@gmail.com>
2017-06-23 06:43:58 +0000

    Remove spacing issue

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -22,5 +22,5 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void super_quick_sort(int *array, int low, int high, size_t size);
-int partition (int *array, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
 #endif

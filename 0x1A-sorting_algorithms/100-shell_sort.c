69b1e24c50e0cd9f95bb1211ebe11a17da3c5c2d
KatyaKalache <katya@kalache.fr>
2017-06-29 22:21:20 +0000

    initial for 100-shell_sort, updating header with shell sort prototype

---
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * shell_sort - sorts an array
+ * @array: the array to sort
+ * @size: size of the array
+ * Return: nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	unsigned int gap;
+	unsigned int i;
+	unsigned int j;
+	int temp;
+
+	gap = size;
+	while (gap != 0)
+	{
+		i = gap;
+		while (i < size)
+		{
+			temp = array[i];
+			j = i;
+			while (j >= gap && temp < array[j - gap])
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+			i++;
+		}
+		gap /= 3 - 1;
+	}
+}

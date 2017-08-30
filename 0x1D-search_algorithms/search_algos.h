519ccb52445a4b7b368bb70ed9c8d26c3d5c758f
Spencer <spencerhcheng@gmail.com>
2017-08-30 21:05:37 +0000

    Add print func

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -2,6 +2,22 @@
 #define SEARCH_H
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+void print_nums(int *array, size_t low, size_t high);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
